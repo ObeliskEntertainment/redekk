df9540cdf5c571b4ef393b2b179487eaf47d0c5e
rasamog <gogo7060@abv.bg>
2015-04-22 16:09:28 +0300

    some progress on upgrading

--- src/re/dekk/Level.java
+++ src/re/dekk/Level.java
@@ -21,6 +21,7 @@ import javafx.stage.Stage;
 public class Level {
     static Ship[][] map;
     static Re_dekk player;
+    static int levelnum;
 
     private static Re_dekk endturn(Re_dekk player) {
         for(Ship[] ss:map){
@@ -40,11 +41,12 @@ public class Level {
     
     static public void startLevel(Stage primaryStage,int level,Re_dekk playe) throws IOException {
         player=playe;
+        levelnum=level;
         
         Pane root = new Pane();
-        Level lvl=LevelSetter.setLevel(level);
-        for(int i=0;i<LevelSetter.levelX (level);i++){
-            for(int j=0;j<LevelSetter.levelY (level);j++){
+        Level lvl=LevelSetter.setLevel(levelnum);
+        for(int i=0;i<LevelSetter.levelX (levelnum);i++){
+            for(int j=0;j<LevelSetter.levelY (levelnum);j++){
                 Button b=new Button();
                 b.setText(lvl.map[i][j].type);
                 b.setLayoutX(50*i);
@@ -60,6 +62,79 @@ public class Level {
                 root.getChildren().add(b);
             }
         }
+        
+        Button upgrade = new Button();
+        upgrade.setText("upgrade");
+        upgrade.setLayoutX(200);
+        upgrade.setLayoutY(450);
+        upgrade.setOnAction((ActionEvent event) -> {
+            Upgrade.startUpgrading(primaryStage,lvl, player);
+        });
+        
+        root.getChildren().add(upgrade);
+        
+        Button exit = new Button();
+        exit.setText("exit game");
+        exit.setLayoutY(450);
+        exit.setOnAction((ActionEvent event) -> {
+            primaryStage.close();
+        });
+        
+        root.getChildren().add(exit);
+        
+        Button nextturn = new Button();
+        nextturn.setText("next turn");
+        nextturn.setLayoutY(450);
+        nextturn.setLayoutX(100);
+        nextturn.setOnAction((ActionEvent event) -> {
+            endturn(player);
+            if(player.mass<=0){
+                System.out.println("YOU HAVE BEEN DESTROYED");
+            }
+            primaryStage.close();
+        });
+        
+        root.getChildren().add(nextturn);
+        
+        Scene scene = new Scene(root, 500, 500);
+        
+        
+        primaryStage.setTitle("Re'dekk");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    static public void resumeLevel(Stage primaryStage,Level level,Re_dekk playe) throws IOException {
+        player=playe;
+        Pane root = new Pane();
+        
+        for(int i=0;i<LevelSetter.levelX (levelnum);i++){
+            for(int j=0;j<LevelSetter.levelY (levelnum);j++){
+                Button b=new Button();
+                b.setText(level.map[i][j].type);
+                b.setLayoutX(50*i);
+                b.setLayoutY(50*j);
+                final Ship ship=level.map[i][j];
+                b.setOnAction((ActionEvent event) -> {
+                    try {
+                        Ship.chooseBattle(primaryStage, ship,player.units);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Level.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                    }
+                });
+                root.getChildren().add(b);
+            }
+        }
+        
+        Button upgrade = new Button();
+        upgrade.setText("upgrade");
+        upgrade.setLayoutX(200);
+        upgrade.setLayoutY(450);
+        upgrade.setOnAction((ActionEvent event) -> {
+            Upgrade.startUpgrading(primaryStage,level, player);
+        });
+        
+        root.getChildren().add(upgrade);
         
         Button exit = new Button();
         exit.setText("exit game");
--- src/re/dekk/Upgrade.java
+++ src/re/dekk/Upgrade.java
@@ -6,10 +6,120 @@
 
 package re.dekk;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  *
  * @author rasamog
  */
 public class Upgrade {
+    
+    static public void startUpgrading(Stage primaryStage,Level lvl,Re_dekk redekk) {
+        Pane root = new Pane();
+        
+        Text mass=new Text();
+        mass.setText("mass= "+Integer.toString(redekk.mass));
+        mass.setLayoutY(10);
+        root.getChildren().add(mass);
+        
+        Button shield = new Button();
+        shield.setText("shields:["+Integer.toString(redekk.shield) +"]cost: "+Integer.toString(redekk.shield*5+3));
+        shield.setLayoutY(50);
+        shield.setOnAction((ActionEvent event) -> {
+            if(redekk.mass>redekk.shield*5+3){
+                redekk.mass-=redekk.shield*5+3;
+                redekk.shield++;
+                startUpgrading(primaryStage,lvl, redekk);
+            }
+        });
+        root.getChildren().add(shield);
+        
+        Button armor = new Button();
+        armor.setText("armor:["+Integer.toString(redekk.armor) +"]cost: "+Integer.toString(redekk.armor*5+3));
+        armor.setLayoutY(100);
+        armor.setOnAction((ActionEvent event) -> {
+            if(redekk.mass>redekk.armor*5+3){
+                redekk.mass-=redekk.armor*5+3;
+                redekk.armor++;
+                startUpgrading(primaryStage,lvl, redekk);
+            }
+        });
+        root.getChildren().add(armor);
+        
+        Button powergeneration = new Button();
+        powergeneration.setText("powergeneration:["+Integer.toString(redekk.powergeneration) +"]cost: "+Integer.toString(redekk.powergeneration*8));
+        powergeneration.setLayoutY(150);
+        powergeneration.setOnAction((ActionEvent event) -> {
+            if(redekk.mass>redekk.powergeneration*8){
+                redekk.mass-=redekk.powergeneration*8;
+                redekk.powergeneration++;
+                startUpgrading(primaryStage,lvl, redekk);
+            }
+        });
+        root.getChildren().add(powergeneration);
+        
+        Button units = new Button();
+        units.setText("units");
+        units.setLayoutY(200);
+        units.setOnAction((ActionEvent event) -> {
+            upgradeUnits(primaryStage, lvl, redekk);
+        });
+        root.getChildren().add(units);
+        
+        Button back = new Button();
+        back.setText("back");
+        back.setLayoutY(450);
+        back.setOnAction((ActionEvent event) -> {
+            try {
+                Level.resumeLevel(primaryStage, lvl, redekk);
+            } catch (IOException ex) {
+                Logger.getLogger(Upgrade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+        });
+        root.getChildren().add(back);
+        
+        Scene scene = new Scene(root, 500, 500);
+        
+        
+        primaryStage.setTitle("Re'dekk");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    static public void upgradeUnits(Stage primaryStage,Level lvl,Re_dekk redekk) {
+        Pane root = new Pane();
+        
+        Text mass=new Text();
+        mass.setText("mass= "+Integer.toString(redekk.mass));
+        mass.setLayoutY(10);
+        root.getChildren().add(mass);
+        
+        
+        Button back = new Button();
+        back.setText("back");
+        back.setLayoutY(450);
+        back.setOnAction((ActionEvent event) -> {
+            try {
+                Level.resumeLevel(primaryStage, lvl, redekk);
+            } catch (IOException ex) {
+                Logger.getLogger(Upgrade.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+        });
+        root.getChildren().add(back);
+        
+        Scene scene = new Scene(root, 500, 500);
+        
+        
+        primaryStage.setTitle("Re'dekk");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
 }
